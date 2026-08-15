b276950ce8fa6a0b529c0df22b931382e5e11830
greyscalee <papanichsan@gmail.com>
2026-08-15 07:07:40 +0700

    feat: setting variable on railway

--- config/database.go
+++ config/database.go
@@ -3,25 +3,39 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "inlinesk8"
-	dbname   = "postgres"
-)
-
 var DB *sql.DB
 
-func ConnectDB() *sql.DB {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
+
+func buildConnectionString() string {
+	if databaseURL := os.Getenv("DATABASE_URL"); databaseURL != "" {
+		return databaseURL
+	}
+
+	host := getEnv("PGHOST", "localhost")
+	port := getEnv("PGPORT", "5432")
+	user := getEnv("PGUSER", "postgres")
+	password := getEnv("PGPASSWORD", "inlinesk8")
+	dbname := getEnv("PGDATABASE", "postgres")
+
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname,
 	)
+}
+
+func ConnectDB() *sql.DB {
+	psqlInfo := buildConnectionString()
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
